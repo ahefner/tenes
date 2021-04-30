592fe55b013331e396643f2566df3469199d55d2
Andy Hefner <ahefner@gmail.com>
2021-04-30 00:14:22 -0400

    Tiny reduction of keyboard input latency
    When the emulated machine strobes the controller shift
    register to read input, immediately pump the event loop
    and peek for keyboard events, processing them if they
    correspond to controller input keys. This eliminates up
    to a frame of input lag in some cases, as games will
    frequently poll for input after vblank is processed,
    at the very end of the emulated frame.
    
    In practice, this case is not hit that often, because
    the emulator is usually running much faster than realtime
    and sleeping at the end of vblank.
    
    This probably won't save me more than a second or two per
    run, but I had to implement in order to measure the benefit..
    
    I should do something similar for joypad inputs, but I'm guessing
    it's not worth the effort.

--- main.c
+++ main.c
@@ -217,11 +217,65 @@ void process_control_key (SDLKey sym)
 }
 
 byte keyboard_input = 0;
+unsigned latency_bypass_count = 0;
+unsigned key_event_count[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+
+// Examine 'key' event. If a mapped controller input, apply and return 1. Otherwise return 0.
+int process_controller_key_event (SDL_KeyboardEvent * key)
+{
+    // Some of the control shortcuts overlap the keyboard
+    // bindings. Ignore these - process_key_event will handle them if
+    // needed.
+    if ((key->keysym.mod & KMOD_CTRL) || (key->keysym.mod & KMOD_ALT)) return 0;
+
+    // Match keysym against keyboard -> button mapping:
+    if (cfg_disable_keyboard) return 0;
+
+    int idx;
+    for (idx = 0; idx < 8; idx++) {
+        if (keymap[idx] == key->keysym.sym) break;
+    }
+
+    if (idx < 8) {
+        switch (key->type) {
+        case SDL_KEYUP:
+            keyboard_input &= ~BIT(idx);
+            key_event_count[idx]++;
+            return 1;
+        case SDL_KEYDOWN:
+            keyboard_input |= BIT(idx);
+            key_event_count[idx]++;
+            return 1;
+        default:
+            printf("??\n");
+            return 0;
+        }
+    }
+
+    return 0;
+}
+
+// Pump event loop and poll for new key events immediately (in
+// response to controller strobe), to reduce input lag.
+void poll_key_events ()
+{
+    if (menu) return;
+    if (nes.machine_type == NSF_PLAYER) return;
+
+    SDL_PumpEvents();
+
+    SDL_Event event;
+    while (1 == SDL_PeepEvents(&event, 1, SDL_PEEKEVENT, SDL_KEYEVENTMASK)
+           && process_controller_key_event(&event.key))
+    {
+        // We processed the key event, so we'd better take it out of the queue..
+        SDL_PeepEvents(&event, 1, SDL_GETEVENT, SDL_KEYEVENTMASK);
+        latency_bypass_count++;
+    }
+}
 
 void process_key_event (SDL_KeyboardEvent * key)
 {
-    int idx;
-
     // NES player keyboard controls.
 
     // FIXME: Current track not saved in state, so wrong at startup or after restore.
@@ -257,6 +311,7 @@ void process_key_event (SDL_KeyboardEvent * key)
 
     // Control and alt keys are available globally.
     // Ignore modified keys down, so as not to confuse the input code.
+    // FIXME: This is part of what makes the control/alt shortcuts feel janky!
     if (((key->keysym.mod & KMOD_CTRL) || (key->keysym.mod & KMOD_ALT))
         && (key->type == SDL_KEYDOWN)) return;
 
@@ -286,25 +341,9 @@ void process_key_event (SDL_KeyboardEvent * key)
     // If the menu is open, pass keystrokes through to it.
     if (menu && menu_process_key_event(key)) return;
 
-    // Match keysym against keyboard -> button mapping:
-    if (cfg_disable_keyboard) idx = 8;
-    else {
-        for (idx = 0; idx < 8; idx++) {
-            if (keymap[idx] == key->keysym.sym) break;
-        }
-    }
+    if (process_controller_key_event(key)) return;
 
-    if (idx < 8) {
-        switch (key->type) {
-        case SDL_KEYUP:
-            keyboard_input &= ~BIT(idx);
-            break;
-        case SDL_KEYDOWN:
-            keyboard_input |= BIT(idx);
-            break;
-        default: break;
-        }
-    } else if (key->type==SDL_KEYUP) {
+    if (key->type==SDL_KEYUP) {
 
         switch (key->keysym.sym) {
         case SDLK_ESCAPE:
@@ -456,7 +495,13 @@ void runframe (void)
     if (0xFF == (frame_times_index & 0xFF))
     {
         long long delta = frame_times_usec[255] - frame_times_usec[0];
-        printf("%0.1f FPS\n", 1.0 / (delta / 256.0 / 1.0e6));
+        printf("%0.1f FPS   %u fast inputs  keys:", 1.0 / (delta / 256.0 / 1.0e6), latency_bypass_count);
+        int total = 0;
+        for (int i=0; i<8; i++) {
+            printf(" %u", key_event_count[i]);
+            total += key_event_count[i];
+        }
+        printf("  total %u\n", total);
     }
     frame_times_index++;
 
